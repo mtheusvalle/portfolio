00b9c02e2817704311510c5053f6cc7b4c5971e8
mtheusvalle <matheusvalle_vni@outlook.com>
2020-11-09 15:48:11 -0300

    primeira página

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+function Home() {
+    return <div>
+        <h2>Home</h2>
+        <Contador />
+    </div>
+}
+
+function Contador() {
+    const [contador,setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador + 1)
+    }
+
+    function zerarContador() {
+        setContador(1)
+    }
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+            <button onClick={zerarContador}>Zerar</button>
+        </div>
+    )
+}
+
+export default Home
